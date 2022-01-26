3843e29cc8e00cdb86c94d426feaebc1da49dcba
Joseph Utecht <thenmal@gmail.com>
2022-01-26 11:31:56 -0600

    added faction display back

--- hereistand/modules/js/Players.js
+++ hereistand/modules/js/Players.js
@@ -12,7 +12,18 @@ define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
     setupPlayers() {
       this.forEachPlayer((player) => {
         // setup gameboard
+        this.place('tplPlayerScore', player, `player_board_${player.id}`);
       });
+    },
+
+    tplPlayerScore(player){
+      return `
+        <div id="globalscore_${player.id}" class="globalscore">
+          <div id="player_score_${player.id}" class="player_score">
+            <span class="meeple echo c${player.color}"></span>
+          </div>
+        </div>
+      `;
     },
   });
 });
