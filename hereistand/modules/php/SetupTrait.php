93d73fa5ef88178287ea0eaa906f15b7f500dd3e
Joseph Utecht <thenmal@gmail.com>
2022-01-29 11:52:41 -0600

    added more units to starting setup

--- hereistand/modules/php/SetupTrait.php
+++ hereistand/modules/php/SetupTrait.php
@@ -4,10 +4,18 @@ namespace HIS;
 trait SetupTrait {
 	public function getSetup() {
 		$setup_base = [
+			HAPSBURG => [
+				BESANCON => [
+					HABSBURG_1UNIT,
+				],
+			],
 			FRANCE => [
 				LYON => [
 					FRANCE_1UNIT,
 					FRANCE_KEY,
+				],
+				TURIN => [
+					FRANCE_HEX,
 				],
 			],
 		];
