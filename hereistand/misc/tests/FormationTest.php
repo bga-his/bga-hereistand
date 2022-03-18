cddd49280b24821d720b92f8064111f7a818088e
Joseph Utecht <thenmal@gmail.com>
2022-03-18 11:16:41 -0500

    basic formation validity tests

--- hereistand/misc/tests/FormationTest.php
+++ hereistand/misc/tests/FormationTest.php
@@ -20,4 +20,41 @@ final class FormationTest extends TestCase {
 		$this->assertInstanceOf(Formation::class, $formation);
 	}
 
+	public function testBasicValidity() {
+		# a formation must contain units
+		$formation = new Formation([]);
+		$this->assertFalse($formation->isValid());
+
+		# a valid formation
+		$tokens = [];
+		$tokens[] = TestingUtils::makeTokenInCity(FERDINAND, VIENNA);
+		$tokens[] = TestingUtils::makeTokenInCity(HAPSBURG_4UNIT, VIENNA);
+		$formation = new Formation($tokens);
+		$this->assertTrue($formation->isValid());
+	}
+
+	public function testCityValidity() {
+		# formations must be in cities
+		$supply_token = TestingUtils::makeToken(HAPSBURG_4UNIT, 'supply_' . HAPSBURG . '_' . HAPSBURG_4UNIT, null);
+		$formation = new Formation([$supply_token]);
+
+		$this->assertFalse($formation->isValid());
+		# all units in a formation must be in the same city
+		$tokens = [];
+		$tokens[] = TestingUtils::makeTokenInCity(FERDINAND, VIENNA);
+		$tokens[] = TestingUtils::makeTokenInCity(HAPSBURG_4UNIT, VIENNA);
+		$tokens[] = TestingUtils::makeTokenInCity(HAPSBURG_4UNIT, ROME);
+		$formation = new Formation($tokens);
+		$this->assertFalse($formation->isValid());
+	}
+
+	public function testPowerValidity() {
+		# all units in formation must be from same major power
+		$tokens = [];
+		$tokens[] = TestingUtils::makeTokenInCity(FERDINAND, VIENNA);
+		$tokens[] = TestingUtils::makeTokenInCity(PAPACY_4UNIT, VIENNA);
+		$formation = new Formation($tokens);
+		$this->assertFalse($formation->isValid());
+	}
+
 }
--- hereistand/modules/php/Models/Formation.php
+++ hereistand/modules/php/Models/Formation.php
@@ -11,4 +11,32 @@ class Formation {
 	public function __construct($tokens) {
 		$this->tokens = $tokens;
 	}
+
+	public function isValid() {
+		# a formation must have units
+		if (Count($this->tokens) == 0) {
+			return false;
+		}
+
+		# all tokens must be in single city
+		if ($this->tokens[0]['location_type'] != 'city') {
+			return false;
+		}
+		$city_id = $this->tokens[0]['location_id'];
+		foreach ($this->tokens as $token) {
+			if ($token['location_id'] != $city_id) {
+				return false;
+			}
+		}
+
+		# all tokens must be from same major power
+		$power = $this->tokens[0]['power'];
+		foreach ($this->tokens as $token) {
+			if ($token['power'] != $power) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
